3752fe86c73330a0a5757f78cf651e9475bc0e30
Yoann-CH <yoann.chambeux@edu.devinci.fr>
2022-11-18 08:41:24 +0100

    first class

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,2 +1,63 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections;
+
+namespace projet
+{
+    class Compte
+    {
+        private int numero;
+        private static int nombreDeCompte = 1;
+        private Personne titulaireCompte;
+
+
+        public Compte(int numero, Personne titulaireCompte)
+        {
+            this.numero = nombreDeCompte;
+            this.titulaireCompte = titulaireCompte;
+            nombreDeCompte++;
+        }
+
+
+        public int Numero
+        {
+            get { return numero; }
+        }
+
+        public Personne TitulaireCompte
+        {
+            get { return titulaireCompte; }
+        }
+    }
+
+    class Personne
+    {
+        private string nom;
+        private int numeroCarteIdentite;
+        private string adresse;
+
+        public Personne(string nom, int numeroCarteIdentite, string adresse)
+        {
+            this.nom = nom;
+            this.numeroCarteIdentite = numeroCarteIdentite;
+            this.adresse = adresse;
+        }
+
+
+    }
+
+    class OperationBancaire
+    {
+        private int numero;
+        private double montant;
+        private string date;
+        private string libelle;
+
+        public OperationBancaire(int numero, float montant, string libelle)
+        {
+            this.numero = numero;
+            this.montant = montant;
+            this.date = DateTime.Now.ToString();
+            this.libelle = libelle;
+        }
+    }
+}
