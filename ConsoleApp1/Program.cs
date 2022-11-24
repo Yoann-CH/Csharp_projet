62d47155bde7a1a01c04aca11de1a8e65fec9a30
Yoann-CH <yoann.chambeux@edu.devinci.fr>
2022-11-24 10:14:21 +0100

    Epargne and Courant add + method

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -3,16 +3,17 @@ using System.Collections;
 
 namespace projet
 {
-    class Compte
+    abstract class Compte
     {
         private int numero;
         private static int nombreDeCompte = 1;
         private Personne titulaireCompte;
+        private List<OperationBancaire> operationBancaires;
 
 
-        public Compte(int numero, Personne titulaireCompte)
+        public Compte(Personne titulaireCompte)
         {
-            this.numero = nombreDeCompte;
+            numero = nombreDeCompte;
             this.titulaireCompte = titulaireCompte;
             nombreDeCompte++;
         }
@@ -27,6 +28,123 @@ namespace projet
         {
             get { return titulaireCompte; }
         }
+
+        public List<OperationBancaire> OperationBancaires
+        {
+            get { return operationBancaires; }
+        }
+
+        public abstract void Crediter(double credit);
+
+        public abstract void Debiter(double debit);
+
+        public void Virement(Compte c1, Compte c2)
+        {
+            double result;
+            double SaisieNombre()
+            {
+                while (!double.TryParse(Console.ReadLine(), out result)) ;
+                return result;
+            }
+            double credit = SaisieNombre();
+            c1.Crediter(credit);
+            double debit = SaisieNombre();
+            c2.Debiter(debit);
+            OperationBancaire o1 = new OperationBancaire(credit, "virement depuis le compte"+c2.Numero);
+            c1.operationBancaires.Add(o1);
+            OperationBancaire o2 = new OperationBancaire(credit, "virement sur le compte" + c1.Numero);
+            c2.operationBancaires.Add(o2);
+        }
+    }
+
+    class Courant:Compte{
+        private double solde;
+        private double decouvertMaximalAutorise;
+        private double debitMaximalAutorise;
+
+        public Courant(Personne titulaireCompte, double solde, double decouvertMaximalAutorise, double debitMaximalAutorise) : base(titulaireCompte)
+        {
+            if(solde < decouvertMaximalAutorise)
+            {
+                Console.WriteLine("Le solde ne peut pas être au dessus du découvert maximal autorisé !");
+                this.solde = 0;
+            }
+            else
+            {
+                this.solde = solde;
+            }
+            this.decouvertMaximalAutorise = decouvertMaximalAutorise;
+            this.debitMaximalAutorise = debitMaximalAutorise;
+        }
+
+        public override void Crediter(double credit)
+        {
+            solde += credit;
+            OperationBancaire o = new OperationBancaire(credit, "compte crédité");
+            OperationBancaires.Add(o);
+        }
+
+        public override void Debiter(double debit)
+        {
+            if(debit > debitMaximalAutorise)
+            {
+                Console.WriteLine("Le montant débité ne peut pas être superieur au débit maximal autorisé !");
+            }
+            else  if(solde - debit < decouvertMaximalAutorise)
+            {
+                Console.WriteLine("Le solde ne peut pas être inférieur au découvert maximal autorisé !");
+            }
+            else
+            {
+                solde -= debit;
+                OperationBancaire o = new OperationBancaire(debit, "compte débité");
+                OperationBancaires.Add(o);
+            }
+        }
+
+    }
+
+    class Epargne : Compte
+    {
+        private double solde;
+        private double interet;
+
+        public Epargne(Personne titulaireCompte, double solde, double interet) : base(titulaireCompte)
+        {
+            if(solde < 0)
+            {
+                Console.WriteLine("Le solde ne peut pas être négatif pour un compte d'épargne");
+                this.solde = 0;
+            }
+            else
+            {
+                this.solde = solde;
+            }
+            this.interet = interet;
+        }
+
+        public override void Crediter(double credit)
+        {
+            solde += credit;
+            OperationBancaire o = new OperationBancaire(credit, "compte crédité");
+            OperationBancaires.Add(o);
+        }
+
+        public override void Debiter(double debit)
+        { 
+            if (solde - debit < 0)
+            {
+                Console.WriteLine("Le solde ne peut pas être inférieur à 0 !");
+            }
+            else
+            {
+                solde -= debit;
+                OperationBancaire o = new OperationBancaire(debit, "compte débité");
+                OperationBancaires.Add(o);
+            }
+        }
+
+
     }
 
     class Personne
@@ -48,15 +166,17 @@ namespace projet
     class OperationBancaire
     {
         private int numero;
+        private int nombreDeOperation = 1;
         private double montant;
         private string date;
         private string libelle;
 
-        public OperationBancaire(int numero, float montant, string libelle)
+        public OperationBancaire(double montant, string libelle)
         {
-            this.numero = numero;
+            numero = nombreDeOperation;
+            nombreDeOperation++;
             this.montant = montant;
-            this.date = DateTime.Now.ToString();
+            date = DateTime.Now.ToString();
             this.libelle = libelle;
         }
     }
